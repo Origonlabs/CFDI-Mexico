112c518905de927658dcea761dadd701a9829023
AGRUFINO GUZMAN GOMEZ <agrufino@cloud.opendex.dev>
2025-07-05 08:53:32 +0000

    PORQUE ME DICE LA BASE DE DATOS NO ESTA CONFIGURADA SI YA LO CONECTE

--- src/lib/neon/client.ts
+++ src/lib/neon/client.ts
@@ -1,4 +1,6 @@
 
+import * as dotenv from 'dotenv';
+dotenv.config();
 import { neon } from '@neondatabase/serverless';
 import { drizzle, type NeonHttpDatabase } from 'drizzle-orm/neon-http';
 import * as schema from '../../../drizzle/schema';
