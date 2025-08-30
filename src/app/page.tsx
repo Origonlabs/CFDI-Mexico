63d117bf7df7c8c3f922a9855e8753851846ff4a
AGRUFINO GUZMAN GOMEZ <agrufino@cloud.opendex.dev>
2025-08-30 22:56:14 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <LoginPage>
    
    with

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,10 @@ import { LoginForm } from "@/components/login-form"
 
 export default function LoginPage() {
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+    <div 
+      className="flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10 bg-cover bg-center"
+      style={{ backgroundImage: "url('/Origon_web_light.webp')" }}
+    >
       <div className="flex w-full max-w-sm flex-col gap-6">
         <a href="#" className="flex items-center justify-center gap-2 self-center font-medium">
           <Image src="/LogoOrigonCFDI.webp" alt="Origon CFDI Logo" width={100} height={27} unoptimized={true} />
