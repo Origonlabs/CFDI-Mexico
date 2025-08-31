6ee8795cd45cba04a77cfbc9205f30a901a980e8
AGRUFINO GUZMAN GOMEZ <agrufino@cloud.opendex.dev>
2025-08-31 19:51:19 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <LoginForm>
    
    with

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -199,6 +199,14 @@ export function LoginForm({
                     )}
                 </div>
               </form>
+               <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+                <span className="bg-card text-muted-foreground relative z-10 px-2">
+                  ¿Eres de una empresa?
+                </span>
+              </div>
+               <Button variant="outline" className="w-full" onClick={() => router.push('/login')} disabled={isSubmitting}>
+                Iniciar Sesión con SSO
+              </Button>
               <div className="text-center text-sm">
                 ¿No tienes una cuenta?{" "}
                 <Link href="/signup" className="underline underline-offset-4">
